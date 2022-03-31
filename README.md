64e952c1435ded3e0cbe1b5ffb54bb35732ee86d
Recep Taylanhan <rcptay@gamil.com>
2022-03-30 23:49:08 -0400

    first commit

--- README.md
+++ README.md
@@ -71,3 +71,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 # github-api-fetch
 # github-api-fetching
 # github-api-fetching
+# github-api-fetch
--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [users1, setUsers1] = useState([]);
+	const [users2, setUsers2] = useState([]);
+	// const [response, setResponse] = useState(null);
+	console.log("our current user:", users1, users2);
+	useEffect(() => {
+		fetch(`https://api.github.com/users`)
+			.then((res) => res.json())
+			.then((users) => {
+				setUsers1(users);
+				setUsers2(users);
+			});
+	}, []);
+	//console.log(users);
+	// async function getUser(username) {
+	//   const response = await fetch(`https://api.github.com/users ${username}`);
+	//   return await response.json();
+	// }
+	// useEffect(async () => {
+	//   const user = await getUser(user);
+	//   setResponse(user);
+	// }, [user]);
+
+	const handleSearch = (e) => {
+		const name = e.target.value;
+		const newUsers = users1.filter((user) => user.login.includes(name));
+		setUsers2(newUsers);
+	};
+
+	return (
+		<div className="App">
+			<h1>GitHub API Fetching</h1>
+			<input onChange={handleSearch} type="text" placeholder="search user" />
+
+			<ul style={{ listStyleType: "none", padding: "0" }}>
+				{users2.map((user) => {
+					return (
+						<li
+							key={user.id}
+							style={{
+								display: "flex",
+								justifyContent: "space-between",
+								border: "1px solid red",
+								backgroundColor: "#134EA4AE",
+							}}
+						>
+							<span>Username: {user.login}</span>
+							<span>Type: {user.type}</span>
+							<img style={{ width: "100px" }} src={user.avatar_url} alt="" />
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
-
-export default App;
